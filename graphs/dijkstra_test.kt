df546d63349773cb10bdfbde53c0bf869a672fab
Lautaro_Lasorsa <lautarolasorsa@gmail.com>
2024-12-25 17:20:15 -0300

    Completado test Dijkstra

--- graphs/dijkstra_test.kt
+++ graphs/dijkstra_test.kt
@@ -35,9 +35,17 @@ fun Dijkstra_test_2(){
     }
 
     for (u in 0 until n){
-        val (min_dist, _) = Dijkstra(grafo, u)
+        val (min_dist, padre) = Dijkstra(grafo, u)
         for (v in 0 until n){
             assert(min_dist[v] == matriz[u][v]) { "Prueba 2.$u.$v fallida" }
+            if (padre[v] != -1){
+                var x = v
+                while (x != u){
+                    val p = padre[x]
+                    assert(matriz[u][v] == matriz[u][p] + matriz[p][v]) { "Prueba 2.$u.$v.$p fallida" }
+                    x = p
+                }
+            }
         }
     }
 }
