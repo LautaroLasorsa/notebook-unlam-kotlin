922c10b020ee28e42a1374603bb98e200f1ccede
Lautaro_Lasorsa <lautarolasorsa@gmail.com>
2024-12-28 20:47:06 -0300

    Corrección de errores en strings

--- strings/lcp.kt
+++ strings/lcp.kt
@@ -1,17 +1,22 @@
-fun computar_lcp(s: String, sa: List<Int>): List<Int>{
+fun computar_lcp(s0: String, sa: List<Int>): MutableList<Int>{
+    val s = s0 + '\u0000'
     val n = s.length
-    val b = MutableList(n+1) { -1 }
-    var j = -1;
-    for (i in 0 until n){
-        while (j >= 0 && s[i] != s[j]) j = b[j]
-        b[i+1] = j
-    }
+    var L = 0
     var lcp = MutableList(n){0}
-    j = 0
+    var plcp = MutableList(n){0}
+    var phi = MutableList(n){0}
+    
+    phi[sa[0]] = -1
+    for (i in 1 until n) phi[sa[i]] = sa[i-1]
     for (i in 0 until n){
-        while (j >= 0 && s[i] != s[sa[j]]) j = b[j]
-        lcp[j] = i
-        j++
+        if (phi[i] == -1){
+            plcp[i] = 0
+            continue
+        }
+        while (s[i+L] == s[phi[i]+L]) L++
+        plcp[i] = L
+        L = maxOf(L-1,0)
     }
+    for (i in 0 until n) lcp[i] = plcp[sa[i]]
     return lcp
 }
