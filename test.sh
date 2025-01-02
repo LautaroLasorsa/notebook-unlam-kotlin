7e183ab7328be9ae33c1290700b5f07b61bb9fa2
Lautaro_Lasorsa <lautarolasorsa@gmail.com>
2025-01-02 15:27:38 -0300

    Corregir bug script de test

--- test.sh
+++ test.sh
@@ -9,7 +9,7 @@ rm -f pruebas_fallidas.txt
 for carpeta in ./*/; do
     cd $carpeta
     for test in *_test.kt; do
-        bash ../run_test.sh $carpeta $test ../output.txt ../pruebas_fallidas.txt
+        bash ../run_test.sh "$carpeta" "$test" "../output.txt" "../pruebas_fallidas.txt"
     done
     cd ..
 done
